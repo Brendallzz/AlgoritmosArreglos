d29482d675f90ebabddcf802b17e4d1aab706a7c
IGCL2345 <lopezignacio2345@gmail.com>
2026-09-11 10:05:18 -0600

    Agregue algoritmo 3 y 4, 7 y 8

--- src/main/java/arreglos/Nivel1.java
+++ src/main/java/arreglos/Nivel1.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package arreglos;
+
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -11,4 +12,37 @@ package arreglos;
  */
 public class Nivel1 {
 
+    public void algoritmo3() {
+
+        int[] numeros = new int[8];
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = Integer.parseInt(
+                    JOptionPane.showInputDialog("Ingrese el número " + (i + 1))
+            );
+        }
+
+        String resultado = "";
+
+        for (int i = 0; i < numeros.length; i++) {
+            resultado += numeros[i] + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, resultado);
+    }
+
+    public void algoritmo4() {
+
+        String palabra = JOptionPane.showInputDialog("Ingrese una palabra");
+
+        char[] letras = palabra.toCharArray();
+
+        String resultado = "";
+
+        for (int i = 0; i < letras.length; i++) {
+            resultado += letras[i] + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, resultado);
+    }
 }
