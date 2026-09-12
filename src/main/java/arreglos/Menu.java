dc513c13b90237fdab486cbecdd0c00f793a4090
Brendall Rodriguez <Brendall.rodriguez@ucr.ac.cr>
2026-09-12 12:32:05 -0600

    Completa algoritmos 1, 2, 5 y 6

--- src/main/java/arreglos/Menu.java
+++ src/main/java/arreglos/Menu.java
@@ -10,5 +10,14 @@ public class Menu {
                 null,
                 "Actividad de algoritmos con arreglos"
         );
+Nivel1 nivel1 = new Nivel1();
+
+nivel1.algoritmo1();
+nivel1.algoritmo2();
+
+Nivel2 nivel2 = new Nivel2();
+
+nivel2.algoritmo5();
+nivel2.algoritmo6();
     }
 }
--- src/main/java/arreglos/Nivel2.java
+++ src/main/java/arreglos/Nivel2.java
@@ -5,10 +5,63 @@
 
 package arreglos;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author brendallnuevo
  */
 public class Nivel2 {
 
+public void algoritmo5(){
+
+double[] numeros = new double[8]; 
+double suma = 0;
+
+for (int i = 0; i < numeros.length; i++){
+    
+        numeros[i] = Double.parseDouble(
+        JOptionPane.showInputDialog(
+                    "ingrese el numero" + (i + 1)
+            )
+        );
+       
+       suma = suma + numeros[i];
+       }
+    double promedio = suma / numeros.length;
+   JOptionPane.showMessageDialog(
+            null,
+            "Promedio " + String.format("%.2f", promedio));
 }
+
+
+public void algoritmo6(){ 
+
+boolean[] asistencia = new boolean[20];
+
+int presentes = 0; //Ahora agregaremos solamente los contadores. Colócalos después del arreglo y antes del for
+int ausentes = 0;
+
+for (int i = 0; i < asistencia.length; i++){
+
+    asistencia[i] = Boolean.parseBoolean(
+            JOptionPane.showInputDialog(null,
+                                    "¿asistio la persona? " + (i + 1) + "? Escriba true o false")
+                                    );
+        if(asistencia[i]){
+          presentes++;
+        }else{  
+             ausentes++;
+}
+}
+   double porcentaje = (presentes * 100.0) / asistencia.length;
+
+        JOptionPane.showMessageDialog(
+                null,
+                "Presentes: " + presentes
+                + "\nAusentes: " + ausentes
+                + "\nPorcentaje de asistencia: "
+                + String.format("%.2f", porcentaje) + "%"
+);
+}
+}//finalgortimo
